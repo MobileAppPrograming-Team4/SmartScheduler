c83346b80f68b78972561b4ab44ee4b79f3222a1
nabivi <nerdring54@gmail.com>
2021-12-01 02:55:24 +0900

    대중교통 이동 노선 출력

--- app/src/main/java/com/example/smartscheduler/Activity/PublicRouteActivity.kt
+++ app/src/main/java/com/example/smartscheduler/Activity/PublicRouteActivity.kt
@@ -1,7 +1,11 @@
 package com.example.smartscheduler.Activity
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -13,31 +17,39 @@ import com.odsay.odsayandroidsdk.ODsayService;
 import com.odsay.odsayandroidsdk.OnResultCallbackListener;
 
 import org.json.JSONObject;
-import net.daum.mf.map.api.MapView
+// import net.daum.mf.map.api.MapView
 
 class PublicRouteActivity : AppCompatActivity() {
 
     lateinit var depPlace: TextView
     lateinit var arrivalPlace: TextView
     lateinit var totalTime: TextView
+    lateinit var startAndFinal: TextView
+    lateinit var stationCount: TextView
+
+    val items = mutableListOf<ListViewItem>()
 
     lateinit var odsayService: ODsayService
     lateinit var jsonObject_path: JSONObject
-    lateinit var jsonObject_lane: JSONObject
+    // lateinit var jsonObject_lane: JSONObject
 
-    lateinit var map: ConstraintLayout
-    val mapView = MapView(this)
+    // lateinit var map: ConstraintLayout
+    // val mapView = MapView(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_publicroute)
 
+        val listView = findViewById<ListView>(R.id.publicRouteListView)
+
         depPlace = findViewById<TextView>(R.id.publicDepPlace)
         arrivalPlace = findViewById<TextView>(R.id.publicArrivalPlace)
         totalTime = findViewById<TextView>(R.id.publicTime)
+        startAndFinal = findViewById<TextView>(R.id.startAndFinal)
+        stationCount = findViewById<TextView>(R.id.stationCount)
 
-        map = findViewById(R.id.publicRouteKakaoMapView)
-        map.addView(mapView)
+        // map = findViewById(R.id.publicRouteKakaoMapView)
+        // map.addView(mapView)
 
         depPlace.setText("경북대학교")
         arrivalPlace.setText("대구광역시청")
@@ -68,9 +80,55 @@ class PublicRouteActivity : AppCompatActivity() {
 
                     totalTime.setText(pathInfo.getInt("totalTime").toString() + "분")
 
-                    val mapObject = pathInfo.getString("mabObj")
+                    // val mapObject = pathInfo.getString("mabObj")
 
-                    publicRouteView(mapObject)
+                    // publicRouteView(mapObject)
+
+                    startAndFinal.setText(pathInfo.getString("firstStartStation") + "→" + pathInfo.getString("lastEndStation"))
+                    stationCount.setText(pathInfo.getInt("totalStationCount").toString() + "개 정류장(역) 이동")
+
+                    val subPathInfo = firstPath.getJSONArray("subPath")
+                    val transferCount = subPathInfo.length()
+
+                    for (i in 0 until transferCount) {
+                        var laneStr = ""
+                        var stationStr = ""
+                        val subPathObj = subPathInfo.getJSONObject(i)
+
+                        if (subPathObj.getInt("trafficType") == 3) {
+                            if (transferCount == 1) {
+                                laneStr = "도보 이동"
+                                items.add(ListViewItem(laneStr, stationStr))
+                            }
+                        }
+                        else {
+                            val laneArray = subPathObj.getJSONArray("lane")
+                            val laneInfo = laneArray.getJSONObject(0)
+                            val passStop = subPathObj.getJSONObject("passStopList")
+                            val passStopArray = passStop.getJSONArray("stations")
+
+                            for (i in 0 until passStopArray.length()) {
+                                val stationInfo = passStopArray.getJSONObject(i)
+                                if (i == passStopArray.length() - 1) {
+                                    stationStr += stationInfo.getString("stationName")
+                                    break;
+                                }
+                                stationStr = stationStr + stationInfo.getString("stationName") + "\n"
+                            }
+
+                            if (subPathObj.getInt("trafficType") == 1) {
+                                laneStr = "지하철 " + laneInfo.getString("name")
+                            }
+                            else if (subPathObj.getInt("trafficType") == 2) {
+                                laneStr = "버스 " + laneInfo.getString("busNo") + "번"
+                            }
+
+                            items.add(ListViewItem(laneStr, stationStr))
+                        }
+
+                    }
+
+                    listView.adapter = MyCustomAdapter(items)
                 }
 
                 override fun onError(code: Int, message: String, api: API) {
@@ -80,6 +138,7 @@ class PublicRouteActivity : AppCompatActivity() {
         )
     }
 
+    /*
     private fun publicRouteView(mabObject: String) {
         odsayService.requestLoadLane(
             mabObject,
@@ -115,5 +174,36 @@ class PublicRouteActivity : AppCompatActivity() {
             }
         )
     }
+    */
+
+    private class MyCustomAdapter(private val items: MutableList<ListViewItem>) : BaseAdapter() {
+        override fun getCount(): Int {
+            return items.size
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getItem(position: Int): Any {
+            return items[position]
+        }
+
+        override fun getView(position: Int, view: View?, viewGroup: ViewGroup?): View {
+            val convertView = LayoutInflater.from(viewGroup?.context)
+            val rowMain = convertView.inflate(R.layout.item_publicroute, viewGroup, false)
+
+            val item: ListViewItem = items[position]
+
+            val laneTextView = rowMain.findViewById<TextView>(R.id.publicNumber)
+            laneTextView.text = item.lane
+            val stationTextView = rowMain.findViewById<TextView>(R.id.routeStation)
+            stationTextView.text = item.station
+
+            return rowMain
+        }
+    }
+
+    data class ListViewItem(val lane: String, val station: String)
 }
 
